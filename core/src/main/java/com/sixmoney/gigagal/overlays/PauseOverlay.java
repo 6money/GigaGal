bec86760830e313229cc667960ce6f8deb1516c8
matt k <mki58@uclive.ac.nz>
2021-06-07 19:22:55 +1200

    Add backdrop to pause text

--- core/src/main/java/com/sixmoney/gigagal/overlays/PauseOverlay.java
+++ core/src/main/java/com/sixmoney/gigagal/overlays/PauseOverlay.java
@@ -21,6 +21,7 @@ public class PauseOverlay extends InputAdapter {
     private Rectangle resume_rect;
     private Rectangle quit_rect;
     private Rectangle restart_rect;
+    private Rectangle text_rect;
 
     public final Viewport viewport;
 
@@ -30,6 +31,7 @@ public class PauseOverlay extends InputAdapter {
         resume_rect = new Rectangle(viewport.getWorldWidth() / 4, viewport.getWorldHeight() / 7, viewport.getWorldWidth() * 3 / 4, viewport.getWorldHeight() / 7);
         restart_rect = new Rectangle(viewport.getWorldWidth() / 4, 0, viewport.getWorldWidth() * 3 / 4, viewport.getWorldHeight() / 7);
         quit_rect = new Rectangle(viewport.getWorldWidth() / 4, 0, viewport.getWorldWidth() * 3 / 4, viewport.getWorldHeight() / 7);
+        text_rect = new Rectangle();
 
         font = new BitmapFont(Gdx.files.internal(Constants.FONT_FILE));
     }
@@ -49,6 +51,11 @@ public class PauseOverlay extends InputAdapter {
         quit_rect.y = 0;
         quit_rect.width = viewport.getWorldWidth() * 2 / 4;
         quit_rect.height = viewport.getWorldHeight() / 7;
+
+        text_rect.x = viewport.getWorldWidth() / 8;
+        text_rect.y = viewport.getWorldHeight() / 1.8f;
+        text_rect.width = viewport.getWorldWidth() * 6 / 8;
+        text_rect.height = viewport.getWorldHeight() / 4f;
     }
 
     public void render(SpriteBatch spriteBatch) {
@@ -60,6 +67,7 @@ public class PauseOverlay extends InputAdapter {
         Assets.get_instance().platformAssets.ninePatch_platform.draw(spriteBatch, restart_rect.x, restart_rect.y, restart_rect.width, restart_rect.height);
         Assets.get_instance().platformAssets.ninePatch_platform.draw(spriteBatch, resume_rect.x, resume_rect.y, resume_rect.width, resume_rect.height);
         Assets.get_instance().platformAssets.ninePatch_platform.draw(spriteBatch, quit_rect.x, quit_rect.y, quit_rect.width, quit_rect.height);
+        Assets.get_instance().platformAssets.ninePatch_platform_hard.draw(spriteBatch, text_rect.x, text_rect.y, text_rect.width, text_rect.height);
 
         font.getData().setScale(1);
         font.draw(spriteBatch, Constants.PAUSED_MESSAGE, viewport.getCamera().viewportWidth / 2, viewport.getCamera().viewportHeight * 3 / 4, 0, Align.center, false);
