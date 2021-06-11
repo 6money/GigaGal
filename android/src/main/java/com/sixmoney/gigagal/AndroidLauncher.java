bba04b94e0421db77af9d6685fab265df64da5b1
matt k <mki58@uclive.ac.nz>
2021-06-12 02:30:42 +1200

    Hide virtual buttons on android

--- android/src/main/java/com/sixmoney/gigagal/AndroidLauncher.java
+++ android/src/main/java/com/sixmoney/gigagal/AndroidLauncher.java
@@ -20,13 +20,14 @@ public class AndroidLauncher extends AndroidApplication {
 
 	private void hideVirtualButtons() {
 		getWindow().getDecorView().setSystemUiVisibility(
-				View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-						| View.SYSTEM_UI_FLAG_FULLSCREEN
-						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+			View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+				| View.SYSTEM_UI_FLAG_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 	}
+
 
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
