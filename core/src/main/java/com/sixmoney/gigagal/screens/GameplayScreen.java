66102057dc5762f4caf73e06cb7495c0ceeb41d1
matt k <mki58@uclive.ac.nz>
2025-04-22 17:01:40 +1200

    Update to LibGDX 1.13.1

--- core/src/main/java/com/sixmoney/gigagal/screens/GameplayScreen.java
+++ core/src/main/java/com/sixmoney/gigagal/screens/GameplayScreen.java
@@ -305,6 +305,11 @@ public class GameplayScreen extends ScreenAdapter implements InputProcessor {
     }
 
     @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         return false;
     }
